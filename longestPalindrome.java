9058a8e7e1527c9068b0dd6e6523626d866fca42
kyleyiyang <kyleyiyang@gmail.com>
2020-11-25 23:41:11 -0500

    Update longestPalindrome.java

--- longestPalindrome.java
+++ longestPalindrome.java
@@ -1,14 +1,17 @@
 class Solution {
     public int longestPalindrome(String s) {
-        Map<Character, Integer> map = new HashMap<>();
+        int[] lower = new int[26];
+        int[] upper = new int[26];
+        
         boolean isOdd=false;
         int len=0;
         for (int j=0;j<s.length();j++) {
-            map.put(s.charAt(j),map.getOrDefault(s.charAt(j),0)+1);
+            if (Character.isLowerCase(s.charAt(j))) lower[s.charAt(j)-'a']++;
+            else upper[s.charAt(j)-'A']++;
         }
-        for (int i:map.values()) {
-            len+=i/2;
-            if (i%2==1) isOdd=true;
+        for (int i=0;i<26;i++) {
+            len+=(lower[i]/2+upper[i]/2);
+            if (lower[i]%2==1 || upper[i]%2==1) isOdd=true;
         }
         return isOdd==true?len*2+1:len*2;
     }
